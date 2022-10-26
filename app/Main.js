41d7d738cab31659cbae5d1b1d2265ec2cb89ec3
LeeStateside <stevenlee@stateside.agency>
2022-10-26 15:50:49 -0600

    adding router

--- app/Main.js
+++ app/Main.js
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 //components
 import Header from "./components/Header";
 import HomeGuest from "./components/HomeGuest";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
 function Main() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
+      <Routes>
+        <Route path="/" element={<HomeGuest />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
