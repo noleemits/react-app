e2923e3ee981c772539975e949de961c7701475c
noleemits <stevenoleemits@gmail.com>
2022-12-02 17:30:28 -0600

    Flash messages

---
+++ app/components/FlashMessages.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+function ComponentName() {
+  return (
+    <div className="floating-alerts">
+      <div className="alert alert-success text-center floating-alert shadow-sm">Hello this is a great message</div>
+    </div>
+  );
+}
+
+export default ComponentName;
